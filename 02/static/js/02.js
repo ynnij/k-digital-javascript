215042589510113230ef87c5a1864c4c382c176b
ynnij <ynnnij@gmail.com>
2023-08-22 13:09:59 +0900

    02 html 및 js 작성

---
+++ 02/static/js/02.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function(){
+    document.getElementById("ah1").innerHTML = "Hello JavaScript";
+    
+});
+
+function hello() {
+    console.log("Hello JS!");
+}
