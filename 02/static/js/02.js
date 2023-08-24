62766fa634067ee4027291cc254f9faa7cac75bc
ynnij <ynnnij@gmail.com>
2023-08-24 13:01:58 +0900

    화살표 함수로 변경

--- 02/static/js/02.js
+++ 02/static/js/02.js
@@ -1,8 +1,17 @@
-document.addEventListener("DOMContentLoaded", function(){
+/* function 키워드로 함수 생성한 경우
+document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("ah1").innerHTML = "Hello JavaScript";
-    
 });
 
 function hello() {
     console.log("Hello JS!");
+}
+*/
+
+// 화살표 함수 
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("ah1").innerHTML ="Hello Javascript!!";
+});
+const hello = () => {
+    console.log("Hello JS!!");
 }
